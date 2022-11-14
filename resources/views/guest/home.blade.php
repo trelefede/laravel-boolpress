2546fd74605ddc8270d4a4c1f29498e045d21ee5
TreleaniFederico <treleani.federico@gmail.com>
2022-11-14 15:41:00 +0100

    add content in home.blade.php add css, js and id=root to conncet by js

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,1 +1,19 @@
-guest
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- CSRF --}}
+    <meta name="csrf-toke" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/front.js') }}" defer></script>
+    <title>{{ config('app.name') }}</title>
+</head>
+
+<body>
+    <div id="root"></div>
+</body>
+
+</html>
