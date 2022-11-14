8f64c01332f0e4179cbec6e488b2efe95d0a577f
TreleaniFederico <treleani.federico@gmail.com>
2022-11-14 15:06:19 +0100

    add fallback route

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ Route::middleware('auth')
         Route::get('/', 'HomeController@index')
         ->name('home');
     });
+
+Route::get("{any?}", function(){
+    return view("guest.home");
+})->where("any", ".*");
