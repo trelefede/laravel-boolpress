1f4e816e4af8b01f3b4d42a991207122de6e05b1
TreleaniFederico <treleani.federico@gmail.com>
2022-11-14 16:07:15 +0100

    add route in protected admins routes

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::middleware('auth')
     ->group(function (){
         Route::get('/', 'HomeController@index')
         ->name('home');
+        Route::resource('posts', 'PostController');
     });
 
 Route::get("{any?}", function(){
