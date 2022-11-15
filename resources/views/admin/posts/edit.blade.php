2f628a4d77b554ad606409f3ddffa92c33c0d281
TreleaniFederico <treleani.federico@gmail.com>
2022-11-15 13:03:09 +0100

    add content in edit.plade.php

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,4 +1,20 @@
 @extends('layouts.dashboard')
 
 @section('content')
+    <form method="POST" action="{{ route('admin.posts.update', $post->id) }}">
+        @csrf
+        @method('PATCH')
+        <div>
+            <label for="title">Titolo:</label>
+            <input type="text" name="title" required minlength="2" maxlength="255"
+                value="{{ old('title', $post->title) }}">
+        </div>
+        <div>
+            <label for="content">Contenuto:</label>
+            <textarea name="content" cols="30" rows="10" required>{{ old('content', $post->content) }}</textarea>
+        </div>
+        <div>
+            <input type="submit" value="Aggiorna">
+        </div>
+    </form>
 @endsection
