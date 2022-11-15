a11d2f5b38ed07a3c6b6a929cde9d019128abe8a
TreleaniFederico <treleani.federico@gmail.com>
2022-11-15 13:12:43 +0100

    moved 'a link' to create new post in index.blade.php

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
     <div class="row">
+        <a href="{{ route('admin.posts.create') }}" class="mb-4">Nuovo Post</a>
+
         @foreach ($posts as $post)
             <div class="col-12">
                 <a href="{{ route('admin.posts.show', $post->id) }}"> {{ $post->title }} </a>
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,5 +4,4 @@
     <h1>{{ $post->title }}</h1>
     <p>{{ $post->content }}</p>
     <a href="{{ route('admin.posts.edit', $post->id) }}" class="mt-3">Modifica</a>
-    <a href="{{ route('admin.posts.create') }}" class="ml-4">Nuovo Post</a>
 @endsection
