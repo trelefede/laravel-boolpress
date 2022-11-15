af8490a297055304dc2cdbdc9dc4cb899be8cf25
TreleaniFederico <treleani.federico@gmail.com>
2022-11-15 13:30:47 +0100

    add @error sections to form input

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -6,10 +6,16 @@
         <div>
             <label for="title">Titolo:</label>
             <input type="text" name="title" required minlength="2" maxlength="255" value="{{ old('title', '') }}">
+            @error('title')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
         <div>
             <label for="content">Contenuto:</label>
             <textarea name="content" cols="30" rows="10" required>{{ old('content', '') }}</textarea>
+            @error('content')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
         <div>
             <input type="submit" value="Posta">
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -8,10 +8,16 @@
             <label for="title">Titolo:</label>
             <input type="text" name="title" required minlength="2" maxlength="255"
                 value="{{ old('title', $post->title) }}">
+            @error('title')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
         <div>
             <label for="content">Contenuto:</label>
             <textarea name="content" cols="30" rows="10" required>{{ old('content', $post->content) }}</textarea>
+            @error('content')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
         <div>
             <input type="submit" value="Aggiorna">
