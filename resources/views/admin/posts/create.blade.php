9ee0a7a1505360daf1503aad909bdf423c4882f9
TreleaniFederico <treleani.federico@gmail.com>
2022-11-15 12:16:43 +0100

    add old() to see previous form data

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -5,11 +5,11 @@
         @csrf
         <div>
             <label for="title">Titolo:</label>
-            <input type="text" name="title" required minlength="2" maxlength="255">
+            <input type="text" name="title" required minlength="2" maxlength="255" value="{{ old('title', '') }}">
         </div>
         <div>
             <label for="content">Contenuto:</label>
-            <textarea name="content" cols="30" rows="10" required></textarea>
+            <textarea name="content" cols="30" rows="10" required>{{ old('content', '') }}</textarea>
         </div>
         <div>
             <input type="submit" value="Posta">
