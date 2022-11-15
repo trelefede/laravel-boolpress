9c20c99d57a20261f2196f33cfcc3f663bcb3899
TreleaniFederico <treleani.federico@gmail.com>
2022-11-15 13:26:02 +0100

    add destroy function

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -133,5 +133,7 @@ class PostController extends Controller
     public function destroy(Post $post)
     {
         //
+        $post->delete();
+        return redirect()->route('admin.posts.index');
     }
 }
