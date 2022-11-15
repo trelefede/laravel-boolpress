18eb60451529d00053b624696f4b0b8a767f4169
TreleaniFederico <treleani.federico@gmail.com>
2022-11-15 12:03:55 +0100

    add return view to function show
    add return view to function create

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -28,6 +28,7 @@ class PostController extends Controller
     public function create()
     {
         //
+        return view('admin.posts.create');
     }
 
     /**
@@ -50,6 +51,7 @@ class PostController extends Controller
     public function show(Post $post)
     {
         //
+        return view('admin.posts.show', compact('post'));
     }
 
     /**
