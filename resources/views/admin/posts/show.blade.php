f93bd69602b2b61882763d8f767f93580453e42e
TreleaniFederico <treleani.federico@gmail.com>
2022-11-15 13:26:22 +0100

    add form to delete/destroy post

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,4 +4,9 @@
     <h1>{{ $post->title }}</h1>
     <p>{{ $post->content }}</p>
     <a href="{{ route('admin.posts.edit', $post->id) }}" class="mt-3">Modifica</a>
+    <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <input type="submit" value="Elimina Post" onclick="confirm('Confermi di voler eliminare il post?')">
+    </form>
 @endsection
