f8390a2939a37d634bd6a5d806897fcfa94317d9
TreleaniFederico <treleani.federico@gmail.com>
2022-11-15 11:44:52 +0100

    corrected route parameter id

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         @foreach ($posts as $post)
             <div class="col-12">
-                <a href="{{ route('admin.posts.show'), $post->id }}"> {{ $post->title }} </a>
+                <a href="{{ route('admin.posts.show', $post->id) }}"> {{ $post->title }} </a>
             </div>
         @endforeach
     </div>
