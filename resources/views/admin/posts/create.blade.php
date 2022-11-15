9923029449703326ecca4829b799f8068da0ebcd
TreleaniFederico <treleani.federico@gmail.com>
2022-11-15 12:15:25 +0100

    add form layout

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,6 +3,16 @@
 @section('content')
     <form method="POST" action="{{ route('admin.posts.store') }}">
         @csrf
-
+        <div>
+            <label for="title">Titolo:</label>
+            <input type="text" name="title" required minlength="2" maxlength="255">
+        </div>
+        <div>
+            <label for="content">Contenuto:</label>
+            <textarea name="content" cols="30" rows="10" required></textarea>
+        </div>
+        <div>
+            <input type="submit" value="Posta">
+        </div>
     </form>
 @endsection
