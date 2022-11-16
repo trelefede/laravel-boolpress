5ee4d333634d1a30a54831b3ef0da98beb5c264b
TreleaniFederico <treleani.federico@gmail.com>
2022-11-16 12:20:23 +0100

    add categories visualization in show.blade.php

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,6 +3,14 @@
 @section('content')
     <h1>{{ $post->title }}</h1>
     <p>{{ $post->content }}</p>
+
+    {{-- categoria --}}
+    @if ($post->category)
+        <p>Categoria:{{ $post->category->name }}</p>
+    @else
+        <p>Nessuna categoria</p>
+    @endif
+
     <a href="{{ route('admin.posts.edit', $post->id) }}" class="mt-3">Modifica</a>
     <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
         @csrf
