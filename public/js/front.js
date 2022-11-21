31871a352f3ddae63ff12eb429f9b1c67eaa8311
TreleaniFederico <treleani.federico@gmail.com>
2022-11-21 15:23:59 +0100

    compiled package-lock.json
    compiled front.js

--- public/js/front.js
+++ public/js/front.js
@@ -49717,8 +49717,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/Fede/Desktop/Boolean/Esercizi-PHP/laravel-many-to-many/resources/js/front.js */"./resources/js/front.js");
-module.exports = __webpack_require__(/*! /Users/Fede/Desktop/Boolean/Esercizi-PHP/laravel-many-to-many/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/Fede/Desktop/Boolean/Esercizi-PHP/laravel-api/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! /Users/Fede/Desktop/Boolean/Esercizi-PHP/laravel-api/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
