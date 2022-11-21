e92e5d93fd3e4e4e07b0f850fe101b5fd13589e3
TreleaniFederico <treleani.federico@gmail.com>
2022-11-21 16:09:09 +0100

    changed route action

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::get('/posts', 'Admin\PostController@index');
+Route::get('/posts', 'Api\PostController@index');
