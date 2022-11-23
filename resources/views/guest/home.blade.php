f5408742de66b02e8171d2754b3016bf0c345677
TreleaniFederico <treleani.federico@gmail.com>
2022-11-23 16:18:13 +0100

    corrected token name

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- CSRF --}}
-    <meta name="csrf-toke" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/front.js') }}" defer></script>
     <title>{{ config('app.name') }}</title>
