48a9db67a5c5468953d6e5e5a9bc9d19700262b0
TreleaniFederico <treleani.federico@gmail.com>
2022-11-24 11:29:36 +0100

    add AboutComponent

---
+++ resources/js/pages/AboutComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h4>Chi Siamo</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AboutComponent',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
