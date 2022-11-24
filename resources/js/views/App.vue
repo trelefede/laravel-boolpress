69118fb362fee361407735ef29f27b4d8733dbf0
TreleaniFederico <treleani.federico@gmail.com>
2022-11-24 11:21:33 +0100

    import NavComponent in App.vue

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
+        <NavComponent />
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import NavComponent from '../components/NavComponent.vue'
 
 export default {
     name: 'App',
+    components: {
+        NavComponent
+    }
 }
 </script>
 
